28385564264397198aa8eb120ffb6eeba90073a3
igarashi339 <igarashi339@gmail.com>
2022-03-26 14:02:45 +0900

    4人フォロー、2人アンフォロー

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 bearer_token = os.getenv("BEARER_TOKEN")
 gas_url = os.getenv("GAS_URL")
 
+SLEEP_SECOND = 10  # 秒
+FOLLOW_USER_COUNT = 4  # 人
+REMOVE_USER_COUNT = 2  # 人
+
 
 def get_client():
     return tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
@@ -21,13 +25,10 @@ def get_client():
 def exec_like(client, user_id):
     """
     最新5件のツイートのうち、いいね数が5以上のものがあればすべていいねする。
-    :param client:
-    :param user_id:
-    :return:
     """
     tweet_list = client.get_users_tweets(id=user_id, max_results=5, exclude="retweets,replies", tweet_fields="public_metrics")
     for tweet in tweet_list.data:
-        time.sleep(10)
+        time.sleep(SLEEP_SECOND)
         id = tweet["id"]
         like_count = tweet["public_metrics"]["like_count"]
         if like_count >= 5:
@@ -38,21 +39,21 @@ def exec_like(client, user_id):
 
 
 def exec_follow(client):
-    data_num = 3
     try:
-        result = urllib.request.urlopen(gas_url + f"?datanum={data_num}")
+        result = urllib.request.urlopen(gas_url + f"?datanum={FOLLOW_USER_COUNT}")
         user_info_list = json.loads(result.read())
     except Exception as e:
         print(e)
     for user_info in user_info_list:
         client.follow_user(target_user_id=user_info["id"])
         exec_like(client, user_info["id"])
-        print(user_info["id"])
-        time.sleep(60)
+        print(f"follow: {id}")
+        time.sleep(SLEEP_SECOND)
 
 
 def exec_remove(client):
     target_user_id = "1449267500454072321"
+    remove_count = 0
     try:
         # フォロー中のユーザが新しい順にとれる
         following_list = client.get_users_following(id=target_user_id, max_results=1000).data
@@ -60,17 +61,20 @@ def exec_remove(client):
         followers_list = client.get_users_followers(id=target_user_id, max_results=1000).data
         followers_id_list = list(map(lambda x: x["id"], followers_list))
         for following_user in following_list_reversed:
+            if remove_count >= REMOVE_USER_COUNT:
+                break
             if following_user["id"] not in followers_id_list:
                 id = following_user["id"]
-                # とりあえずリムる人数が1人なのですぐbreakする
                 client.unfollow_user(target_user_id=id)
-                break
+                print(f"remove: {id}")
+                remove_count += 1
+                time.sleep(SLEEP_SECOND)
     except Exception as e:
         print(e)
 
 
 def main(client):
-    exec_follow(client)
+    # exec_follow(client)
     exec_remove(client)
 
 
